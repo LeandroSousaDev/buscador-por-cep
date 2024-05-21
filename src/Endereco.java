761b577ef58e01a12daa09cbf2bd3ea098f7d291
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-05-21 11:45:39 -0300

    projeto pronto

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,3 +1,3 @@
-public record Indereco(String logradouro, String cep, String localidade,
+public record Endereco(String logradouro, String cep, String localidade,
                        String complemento, String uf) {
 }
